31289ce8cee61207b455bc7777c893e9fb01ab42
behrd <behrd@indiana.edu>
2017-07-26 01:09:32 -0400

    completed backend authentication

--- api/web/router.ex
+++ api/web/router.ex
@@ -1,11 +1,33 @@
 defmodule Fntn.Router do
   use Fntn.Web, :router
 
+pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
+  end
+
+  scope "/", Fntn do
+    pipe_through :browser
+
+    get "/", PageController, :index
   end
 
   scope "/api", Fntn do
     pipe_through :api
-  end
+
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionController, :delete
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserController, only: [:create]
+ end
+
 end
--- api/web/views/user_view.ex
+++ api/web/views/user_view.ex
@@ -1,18 +1,10 @@
 defmodule Fntn.UserView do
   use Fntn.Web, :view
 
-  def render("index.json", %{users: users}) do
-    %{data: render_many(users, Fntn.UserView, "user.json")}
-  end
-
-  def render("show.json", %{user: user}) do
-    %{data: render_one(user, Fntn.UserView, "user.json")}
-  end
-
   def render("user.json", %{user: user}) do
     %{id: user.id,
       username: user.username,
       email: user.email,
-      password_hash: user.password_hash}
+     }	
   end
 end
